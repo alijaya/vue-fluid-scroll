6a31991f07b51cc718a60680e85d9af51172d7ac
Ali Jaya Meilio <alijayameilio@gmail.com>
2018-07-24 06:15:23 +0200

    build

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SmoothScroll class="smooth-scroll">
+      <div class="long-text">
+        <ol>
+          <li v-for="n in 200" :key="n">I have {{n}} doggo!</li>
+        </ol>
+      </div>
+    </SmoothScroll>
+    <SmoothScroll class="horizontal-scroll" :invert="true">
+      <div class="long-text">
+        <p class="horizontal-text">
+          <span v-for="n in 200" :key="n">I have {{n}} doggo!</span>
+        </p>
+      </div>
+    </SmoothScroll>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SmoothScroll from './components/SmoothScroll'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    SmoothScroll,
+  },
 }
 </script>
 
 <style>
+html, body, #app {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
+
+</style>
+
+<style scoped>
 #app {
+  position: relative;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.smooth-scroll {
+  position: absolute;
+  top: 0%;
+  left: 0%;
+  width: 50%;
+  height: 100%;
+}
+
+.horizontal-scroll {
+  position: absolute;
+  top: 0%;
+  left: 50%;
+  width: 50%;
+  height: 100%;
+}
+
+.content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+}
+
+.long-text {
+  padding: 3em;
+}
+
+.horizontal-text {
+  width: 2000px;
+}
+
 </style>
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  baseUrl: "/smooth-scroll/",
+  outputDir: "/docs/"
+}
